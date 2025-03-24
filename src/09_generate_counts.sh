6ef5e77eef31e1174e63f05ff85547ef5d685278
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-03-24 17:13:25 -0400

    Accomodated both timepoints in featureCounts
    I realized that previously the code combined the data for both timepoints into a single count file
    
    Now it generates two outputs, one for each timepoint

--- src/09_generate_counts.sh
+++ src/09_generate_counts.sh
@@ -1,26 +1,52 @@
-# 03_generate_counts.sh
 #!/bin/bash
 
 generate_counts() {
     WORKING_DIRECTORY="$(pwd)"
-    BAM_FILES=()
+    OUTPUT_DESTINATION="${WORKING_DIRECTORY}/data/quant"
+    TIMEPOINT_1_DIRECTORY="${OUTPUT_DESTINATION}/1h"
+    TIMEPOINT_2_DIRECTORY="${OUTPUT_DESTINATION}/8h"
+    TIMEPOINT_1_OUTPUT_DATA_PATH="${TIMEPOINT_1_DIRECTORY}/output_data.txt"
+    TIMEPOINT_1_CLEAN_COUNTS_PATH="${TIMEPOINT_1_DIRECTORY}/clean_counts.txt"
+    TIMEPOINT_2_OUTPUT_DATA_PATH="${TIMEPOINT_2_DIRECTORY}/output_data.txt"
+    TIMEPOINT_2_CLEAN_COUNTS_PATH="${TIMEPOINT_2_DIRECTORY}/clean_counts.txt"
+
+    TIMEPOINT_1_BAM_FILES=()
+    TIMEPOINT_2_BAM_FILES=()
+
+    REFERENCE_GENOME="Arabidopsis_thaliana.TAIR10.55.gtf"
+
     
-    # Ensure the directories exist
-    mkdir -p "${WORKING_DIRECTORY}/data/quant"
+    # Ensure the output directory and files exist
+    mkdir -p "${OUTPUT_DESTINATION}"
+    mkdir -p "${TIMEPOINT_1_DIRECTORY}"
+    mkdir -p "${TIMEPOINT_2_DIRECTORY}"
+    touch "${TIMEPOINT_1_OUTPUT_DATA_PATH}" "${TIMEPOINT_1_CLEAN_COUNTS_PATH}"
     
     for bam in "${WORKING_DIRECTORY}/data/alignments/"*.bam; do
-        BAM_FILES+=("$bam")
+        # Get the third character of the filename (excluding path)
+        third_char="${bam##*/}"
+        third_char="${third_char:2:1}"
+        
+        # Add to appropriate timepoint array based on third character
+        case "$third_char" in
+            0|1) TIMEPOINT_1_BAM_FILES+=("$bam") ;;
+            *)   TIMEPOINT_2_BAM_FILES+=("$bam") ;;
+        esac
     done
     
-    # Initialize output files if they do not exist
-    touch "${WORKING_DIRECTORY}/data/quant/output_data.txt" "${WORKING_DIRECTORY}/data/quant/clean_counts.txt"
+    featureCounts -p -a "${REFERENCE_GENOME}" \
+        -o "${TIMEPOINT_1_OUTPUT_DATA_PATH}" \
+        "${TIMEPOINT_1_BAM_FILES[@]}"
     
-    featureCounts -p -a Arabidopsis_thaliana.TAIR10.55.gtf \
-        -o "${WORKING_DIRECTORY}/data/quant/output_data.txt" \
-        "${BAM_FILES[@]}"
-    
-    cut -f1,7- "${WORKING_DIRECTORY}/data/quant/output_data.txt" > \
-        "${WORKING_DIRECTORY}/data/quant/clean_counts.txt"
+    cut -f1,7- "${TIMEPOINT_1_OUTPUT_DATA_PATH}" > \
+        "${TIMEPOINT_1_CLEAN_COUNTS_PATH}"
+
+    featureCounts -p -a "${REFERENCE_GENOME}" \
+    -o "${TIMEPOINT_2_OUTPUT_DATA_PATH}" \
+    "${TIMEPOINT_2_BAM_FILES[@]}"
+
+    cut -f1,7- "${TIMEPOINT_2_OUTPUT_DATA_PATH}" > \
+        "${TIMEPOINT_2_CLEAN_COUNTS_PATH}"
 }
 
 generate_counts "$@"
