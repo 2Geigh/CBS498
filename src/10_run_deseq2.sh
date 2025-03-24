f8359b38530f4a803dc6b709256f67b20a9a219c
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-03-24 16:34:34 -0400

    added deseq2 pipeline

--- src/10_run_deseq2.sh
+++ src/10_run_deseq2.sh
@@ -1,12 +1,13 @@
-# 04_run_deseq2.sh
 #!/bin/bash
 
 run_deseq2() {
     WORKING_DIRECTORY="$(pwd)"
-    Rscript << EOF
-    # Existing R code remains unchanged
-    # R automatically handles quoted paths from bash
-EOF
+
+    Rscript ./src/deseq2.r
 }
 
-run_deseq2 "$@"
+main() {
+    run_deseq2 "$@"
+}
+
+main "$@"
